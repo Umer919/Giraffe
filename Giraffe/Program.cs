7fcb3aaf7e55e0924bd3223fe5fe883edb44e4ea
Umer Satti <umersatti919@gmail.com>
2020-12-26 12:01:22 +0500

    first commit

---
+++ Giraffe/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giraffe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            String characterName = "Satti";
+            int characterAge;
+            characterAge = 19;
+
+
+
+            Console.WriteLine(" There was a boy once named " + characterName);
+            Console.WriteLine(" He was " + characterAge + "  years old ");
+
+            characterName = "Mike";
+            characterAge = 22;
+
+            Console.WriteLine(" He really liked the name " + characterName);
+            Console.WriteLine(" But he didn't like the Age  " + characterAge);
+            
+
+        }
+    }
+}
